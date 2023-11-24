a25786fcbfc88793316d4679ea6a84b9a5362d05
Leonardo Moya <88905913+lsmcba@users.noreply.github.com>
2023-11-24 02:28:23 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+//create web server
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//serve static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//import the routes
+const routes = require('./routes.js');
+
+//use the routes
+app.use('/', routes);
+
+//start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
