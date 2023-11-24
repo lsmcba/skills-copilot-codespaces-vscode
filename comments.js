276bb9344a94e7e148337bf38ba596d9f67e44f9
Leonardo Moya <88905913+lsmcba@users.noreply.github.com>
2023-11-24 03:08:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,90 +1,37 @@
 //create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/comments');
+var Comment = require('./model/comment');
+var cors = require('cors');
 
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/comment', function(req, res) {
+    var comment = new Comment(req.body);
+    comment.save(function(err, savedComment) {
+        if (err) {
+            res.status(500).send({error: "Your comment was not saved."});
+        } else {
+            res.send(savedComment);
+        }
+    });
 });
 
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/comment', function(req, res) {
+    Comment.find({}, function(err, comments) {
+        if (err) {
+            res.status(500).send({error: "Could not fetch comments."});
+        } else {
+            res.send(comments);
+        }
+    });
 });
 
-// Path: comments.js
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-// Path: comments.js
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-// Path: comments.js
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-// Path: comments.js
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-// Path: comments.js
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//listen on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3001, function() {
+    console.log("Server is running on port 3001...");
 });
